6a4e773fa461f7dcd660812fe9ccd3a2ed259dd5
Pushkar Tiwari <201851095@iiitvadodara.ac.in>
2020-09-24 11:46:06 +0530

    made some changes in models.py

--- td/models.py
+++ td/models.py
@@ -1,12 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import User
+from datetime import datetime
 
 # Create your models here.
+
+
 class Todo(models.Model):
     title = models.CharField(max_length=100)
     memo = models.TextField(blank=True)
     created = models.DateTimeField(auto_now_add=True)
-    datecompleted = models.DateTimeField(null= True)
+    datecompleted = models.DateTimeField(null=True, blank=True)
     importanat = models.BooleanField(default=False)
-    user = models.ForeignKey(User, on_delete= models.CASCADE)
-    
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+def __str__(self):
+    return self.title
